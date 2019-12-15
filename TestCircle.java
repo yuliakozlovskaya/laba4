f24af2de941196a8906d29196431332860cdf017
yuliakozlovskaya <49129572+yuliakozlovskaya@users.noreply.github.com>
2019-12-15 23:12:02 +0300

    Add files via upload

--- TestCircle.java
+++ TestCircle.java
@@ -1,17 +1,15 @@
-public class TestCircle {
-    import java.lang.*;
+import java.lang.*;
 import java.util.Scanner;
-import com.company.Circle;
 
-    public class Tester {
-        public static void main(String[] args) {
-            double r;
-            Circle k1 = new Circle(2.5, 2.6, 2.9, "pink");
-            System.out.println("Длина окружности = " + k1.getLength() + "см\n");
-            Scanner source = new Scanner(System.in);
-            System.out.println("Пожалуйста, введите радиус ");
-            r = source.nextDouble();
-            k1.setR(r);
-            System.out.println("\nДлина окружности равна " + k1.getLength() + "см");
-        }
+public class TestCircle {
+    public static void main(String[] args) {
+        double r;
+        Circle k1 = new Circle(7.1, 8.1, 9.1, "pink");
+        System.out.println("ДО = " + k1.getLength() + "см\n");
+        Scanner source = new Scanner(System.in);
+        System.out.println("Введите радиус ");
+        r = source.nextDouble();
+        k1.setR(r);
+        System.out.println("\nДлина окружности = " + k1.getLength() + "см");
+    }
 }
